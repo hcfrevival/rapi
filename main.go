4edc9992f9ddb6d1e75355f5fffe790dcd194496
j-rockwell <john@llewkcor.com>
2023-05-26 15:56:05 -0700

    main: docs

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	// conf init
 	conf := config.Prepare()
 	gin.SetMode(conf.Gin.Mode)
 
@@ -17,18 +18,22 @@ func main() {
 		panic("failed to establish mongo connection: " + err.Error())
 	}
 
+	// middleware
 	router := gin.New()
 	router.Use(gin.Recovery())
 	router.Use(gin.Logger())
 
+	// route controller, passes mongo + db name to children
 	rc := routers.RouteController{
 		Mongo:        mongoClient,
 		DatabaseName: conf.Mongo.DatabaseName,
 	}
 
+	// routes
 	rc.ApplyLeaderboardRoutes(router)
 	rc.ApplyFeedRoutes(router)
 
+	// start gin
 	err = router.Run(":" + conf.Gin.Port)
 	if err != nil {
 		panic("failed to start gin: " + err.Error())
