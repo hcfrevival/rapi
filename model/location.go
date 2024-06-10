81109006fae37153b50c3521497bc491cdd1fe66
j-rockwell <john@llewkcor.com>
2024-06-09 17:09:08 -0700

    main: omitempty for location struct

--- model/location.go
+++ model/location.go
@@ -1,8 +1,8 @@
 package model
 
 type Location struct {
-	World string  `json:"world" bson:"world"`
-	X     float32 `json:"x" bson:"x,truncate"`
-	Y     float32 `json:"y" bson:"y,truncate"`
-	Z     float32 `json:"z" bson:"z,truncate"`
+	World string  `json:"world,omitempty" bson:"world,omitempty"`
+	X     float32 `json:"x" bson:"x,omitempty,truncated"`
+	Y     float32 `json:"y" bson:"y,omitempty,truncate"`
+	Z     float32 `json:"z" bson:"z,omitempty,truncate"`
 }
